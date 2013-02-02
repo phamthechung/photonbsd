f9ef375388f2443a2dac25edd19ff4311bf76849
Pham The Chung <phamthechung0@gmail.com>
2013-02-02 13:57:57 +0800

    remove all

--- GENERIC/vers.c
+++
@@ -1,2 +0,0 @@
-char sccs[] = "@(#)4.3 BSD #1: Fri Jun  6 19:55:29 PDT 1986 (karels@monet.Berkeley.EDU:/usr/src/sys/GENERIC)\n";
-char version[] = "4.3 BSD UNIX #1: Fri Jun  6 19:55:29 PDT 1986\n    karels@monet.Berkeley.EDU:/usr/src/sys/GENERIC\n";
